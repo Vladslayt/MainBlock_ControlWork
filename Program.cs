0ad2577cc0791388797ca23ad73f70c7f740dcc0
Vladislav <kasyan.vladik125@mail.ru>
2023-12-26 18:06:28 +0300

    Add solve

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Выберете способ заполнения массива: \n" +
+                "   1. Ввести c клавиатуры \n" +
+                "   2. Использовать заданный на старте");
+
+            int value = int.Parse(Console.ReadLine()!);
+            string[] arr;
+
+            if (value == 1)
+                arr = Console.ReadLine()!.Split(" ");
+            else
+                arr = new string[] { "Hello", "2", "world", ":-)"};
+
+            string[] newArr = FilterStrings(arr);
+            Console.WriteLine(string.Join(", ", newArr));
+    }
+    
+    static string[] SimpleFilterStrings(string[] arr)
+    {
+        return arr.Where(s => s.Length <= 3).ToArray();
+    }
+
+    static string[] FilterStrings(string[] arr)
+    {
+        int countElem = 0;
+        foreach (string el in arr)
+            if (el.Length <= 3)
+                countElem++;
+        
+        string[] newArr = new string[countElem];
+        int index = 0;
+        foreach (string el in arr)
+        {
+            if (el.Length <= 3)
+            {
+                newArr[index] = el;
+                index++;
+            }
+        }
+
+        return newArr;
+    }
+}
